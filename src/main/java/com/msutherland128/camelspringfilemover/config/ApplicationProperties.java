a61e7fceb2e79ff34a39b6a872b1b5dc27ccd420
Mike Sutherland <michael.sutherland@nuffieldhealth.com>
2022-09-21 17:23:55 +0100

    Added spring annotations and code to ApplicationProperties and PrintValues classes.

--- src/main/java/com/msutherland128/camelspringfilemover/config/ApplicationProperties.java
+++ src/main/java/com/msutherland128/camelspringfilemover/config/ApplicationProperties.java
@@ -1,7 +1,35 @@
 package com.msutherland128.camelspringfilemover.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("ih")
 public class ApplicationProperties {
 
+    // Create application properties variables
+    private String inFileDirectory;
+    private String outFileDirectory;
+
+    // getters & setters
+    public String getInFileDirectory(){
+        return inFileDirectory;
+    }
+
+    public void setInFileDirectory(final String inFileDirectory){
+        this.inFileDirectory = inFileDirectory;
+
+    }
+
+    public String getOutFileDirectory(){
+        return outFileDirectory;
+    }
+
+    public void setOutFileDirectory(final String outFileDirectory){
+        this.outFileDirectory = outFileDirectory;
+    }
 
 
 }
+
+
--- src/main/java/com/msutherland128/camelspringfilemover/processor/PrintValues.java
+++ src/main/java/com/msutherland128/camelspringfilemover/processor/PrintValues.java
@@ -1,5 +1,30 @@
 package com.msutherland128.camelspringfilemover.processor;
 
+import com.msutherland128.camelspringfilemover.config.ApplicationProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PrintValues {
+
+    private ApplicationProperties applicationProperties;
+
+    // Create a constructor which runs a method using variables from application properties
+
+    @Autowired
+    public PrintValues(){
+        this.applicationProperties = new ApplicationProperties();
+        printAllValues();
+    }
+
+
+    private void printAllValues(){
+        String inFileValue = applicationProperties.getInFileDirectory();
+        String outFileValue = applicationProperties.getOutFileDirectory();
+
+        System.out.println(inFileValue);
+        System.out.println(outFileValue);
+
+    }
 
 }
