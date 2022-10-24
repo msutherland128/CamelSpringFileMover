7471d4f0d2fd7daaacf2938fa3155d01b3b8d06a
Mike Sutherland <michael.sutherland@nuffieldhealth.com>
2022-10-24 16:34:40 +0100

    Added logging level details to application.properties. Updated route with logging code.

--- src/main/java/com/msutherland128/camelspringfilemover/routes/FileMoverRoute.java
+++ src/main/java/com/msutherland128/camelspringfilemover/routes/FileMoverRoute.java
@@ -1,6 +1,7 @@
 package com.msutherland128.camelspringfilemover.routes;
 
 import com.msutherland128.camelspringfilemover.config.ApplicationProperties;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Component;
 public class FileMoverRoute extends RouteBuilder {
 
     private ApplicationProperties applicationProperties;
+    private int counter = 0;
 
     public FileMoverRoute(ApplicationProperties applicationProperties){
         this.applicationProperties = applicationProperties;
@@ -17,8 +19,19 @@ public class FileMoverRoute extends RouteBuilder {
     public void configure() throws Exception {
 
         from(applicationProperties.getInTextFileDirectory())
+                .log(LoggingLevel.INFO, "Received message with body: ${body}")
+                .setHeader("testHeader", simple("1234"))
+                .process(exchange -> {
+                    exchange.getIn().setHeader("counter", counter++);
+                    exchange.setProperty("testProperty", "testPropertyValue");
+                })
+                .log(LoggingLevel.INFO, "${headers}")
+                .log(LoggingLevel.INFO, "Number of files received is: ${header.counter}")
+                .log(LoggingLevel.INFO, "Message property is: " + exchangeProperty("testProperty"))
                 // Todo - research labels
                 //.id("textFileRoute")
+                // Todo - investigate issue with debug logging low priority
+                .log(LoggingLevel.DEBUG, "Sending file to: {}", applicationProperties.getOutTextFileDirectory())
                 .to(applicationProperties.getOutTextFileDirectory());
 
         // todo - complete route for csv files
