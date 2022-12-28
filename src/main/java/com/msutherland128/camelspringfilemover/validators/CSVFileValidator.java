c0eb36dcd2d6e0805365aba38a6f2f6ee4c15763
Mike Sutherland <michael.sutherland@nuffieldhealth.com>
2022-12-28 16:20:15 +0000

    Updated headerStripper method in CSVFileValidator class.

--- src/main/java/com/msutherland128/camelspringfilemover/validators/CSVFileValidator.java
+++ src/main/java/com/msutherland128/camelspringfilemover/validators/CSVFileValidator.java
@@ -4,12 +4,9 @@ import com.msutherland128.camelspringfilemover.config.ApplicationProperties;
 import com.msutherland128.camelspringfilemover.exceptions.ValidationException;
 import com.msutherland128.camelspringfilemover.processors.CSVFileProcessor;
 import com.msutherland128.camelspringfilemover.processors.MessageParser;
-import org.apache.logging.log4j.core.util.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
 
 @Component
 public class CSVFileValidator extends FileValidator {
@@ -18,7 +15,7 @@ public class CSVFileValidator extends FileValidator {
     private final MessageParser messageParser;
     private final ApplicationProperties applicationProperties;
 
-    public CSVFileValidator (final MessageParser messageParser, ApplicationProperties applicationProperties) {
+    public CSVFileValidator(final MessageParser messageParser, ApplicationProperties applicationProperties) {
         this.messageParser = messageParser;
         this.applicationProperties = applicationProperties;
     }
@@ -26,11 +23,9 @@ public class CSVFileValidator extends FileValidator {
     public void validate(String message) throws ValidationException {
         String[] csvRows = messageParser.parseCSVContents(message);
 
-        // Todo - validate that column names are in the correct order and named correctly
-
         validateColumnHeaders(csvRows[0]);
 
-        // Todo - Create a loop that passes each line of the csv row and send these to the body validator. Don't revalidate 0 index start index of loop at 1.
+        // Todo - pass data to a method with a loop that passes each line of the csv row and send these to the body validator. Don't revalidate 0 index start index of loop at 1.
         // for loop here
 
     }
@@ -49,35 +44,37 @@ public class CSVFileValidator extends FileValidator {
         // Testing output of get method
         LOGGER.info("getCsvColumnHeaders logged: " + applicationProperties.getCsvColumnHeaders());
 
-        // Todo - Ask Grant if csvColumnHeaders should be converted to String array list within  method so column order can be compared
+        String applicationCsvHeaders = applicationProperties.getCsvColumnHeaders();
 
-        // Todo - compare the headers against application properties. Consider using trim/strip methods
+        LOGGER.info("Sending to headerStripper method.");
+        headerStripper(headerRow);
 
-        // Todo - call private strip method here pass headerRow & applicationproperties getter
+    }
+
+
+    private void headerStripper(String headerRow) {
 
         if (headerRow
                 .strip()
-                .trim().replaceAll("\n","")
-                    .replaceAll("\r","")
-                    .replaceAll("\t","")
-                    .replaceAll("\\s", "")
-                    .replaceAll("\uFEFF", "")
+                .trim().replaceAll("\n", "")
+                .replaceAll("\r", "")
+                .replaceAll("\t", "")
+                .replaceAll("\\s", "")
+                .replaceAll("\uFEFF", "")
                 .equalsIgnoreCase(applicationProperties.getCsvColumnHeaders()
                         .strip()
                         .trim()
-                        .replaceAll("\n","")
-                                .replaceAll("\r","")
-                                .replaceAll("\t","")
-                                .replaceAll("\\s", "")
-                                .replaceAll("\uFEFF", "")
+                        .replaceAll("\n", "")
+                        .replaceAll("\r", "")
+                        .replaceAll("\t", "")
+                        .replaceAll("\\s", "")
+                        .replaceAll("\uFEFF", "")
                 )) {
             LOGGER.info("Validated successfully.");
 
         } else {
-            LOGGER.info("Validation failed.");
+            LOGGER.info("Validation failed. Please check csv file headers.");
         }
-
     }
 
-    // Todo - Put all of the strip / trim in from validateColumnHeaders method into private method here
 }
