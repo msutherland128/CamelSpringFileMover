13e4e14790cb37927487f1e15e42e5bb9bacc8b2
Mike Sutherland <michael.sutherland@nuffieldhealth.com>
2023-02-20 12:32:26 +0000

    Made updates to CSVFileProcessor method

--- src/main/java/com/msutherland128/camelspringfilemover/processors/CSVFileProcessor.java
+++ src/main/java/com/msutherland128/camelspringfilemover/processors/CSVFileProcessor.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class CSVFileProcessor implements Processor {
 
@@ -29,18 +31,54 @@ public class CSVFileProcessor implements Processor {
         // Todo - bring in CSVFileValidator and validate contents
         csvFileValidator.validate(csvMessageBody);
 
-        csvMessageBodyManipulator(exchange, csvMessageBody);
+        csvMessageBodyManipulator(exchange);
 
     }
 
-    private void csvMessageBodyManipulator(Exchange exchange, String csvMessageBody) {
-        // Customer ID	Customer Notes	Address	DOB	Gender	Name
+    private void csvMessageBodyManipulator(Exchange exchange) {
+        // Customer ID,	Customer Notes,	Address,	DOB,	Gender,	Name
         LOGGER.info("Starting message manipulation on message body.");
 
         // Todo - split message into an array. Bring in MessageParser & use the message parser method
-
+        String[] csvRows = messageParser.parseCSVContents(exchange.getIn().getBody(String.class));
+        String[] updatedCSVRows = new String[csvRows.length];
+//        String[] csvColumns = messageParser.parseCSVRow(csvRows[1]);
+        StringBuilder builder = new StringBuilder();
 
         // Todo - concat GP details to customer notes
+        for (int i = 0; i < csvRows.length; i++ ) {
+
+            // Ignores the header row
+            if (i > 0) {
+                System.out.println("Looping message body. Concatenating GP details:");
+                String[] csvColumns = messageParser.parseCSVRow(csvRows[i]);
+                String customerNotes = csvColumns[1].concat(" Some GP data");
+
+                System.out.println(csvRows[i]);
+
+                for (int j = 0; j < csvColumns.length; j++) {
+                    // Inner loop to update the Customer Notes column with customerNotes var
+                    if (j == 1) {
+                        builder.append(customerNotes);
+                        System.out.println(customerNotes);
+                        builder.append(",");
+                    }
+                    builder.append(csvColumns[j]);
+                    builder.append(",");
+                    System.out.println(csvColumns[j]);
+                }
+                System.out.println("Print builder: ");
+                System.out.println(builder.toString());
+                updatedCSVRows[i] = builder.toString();
+                builder.setLength(0);
+
+            }
+
+        }
+
+        for (String line : updatedCSVRows) {
+            System.out.println(line);
+        }
 
         // Todo - set data to message body
 //        String manipulatedCSVMessageBody = csvMessageBody.concat(" - This message has been manipulated by the csvMessageBodyManipulator method.");
